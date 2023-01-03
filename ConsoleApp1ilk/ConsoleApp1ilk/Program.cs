d9543a51f5ed79b97a47b943f9d1661a763cc11d
Barış Demiroğ <barisdemirogyt@gmail.com>
2023-01-03 10:54:42 +0300

    kullanıcıverigirişi ve bar kontrol
    Kullanıcıdan isim soyisim ve yaş isteyen yaşının 18 den büyük veya küçük oldugunu kontrol eden bir app

---
+++ ConsoleApp1ilk/ConsoleApp1ilk/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1ilk
+{
+    internal class Program
+    {
+        static void Main(string[] args )
+        {
+            Console.WriteLine("BARZO BAR'A HOŞ GELDİNİZ");
+
+
+            Console.WriteLine("İsminizi girin");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Soyisminizi girin");
+            string soyad = Console.ReadLine();
+
+            Console.WriteLine("Son olarak yaşınız nedir?");
+            int yas = Convert.ToInt16(Console.ReadLine());
+
+            if (yas >= 18)
+
+                Console.WriteLine("Girebilirsiniz Efendim");
+
+            else
+
+            Console.WriteLine("Giremezsiniz Efendim, maalesef yaşınız 18 den küçük");
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
